c99e4d329860ef22d8be3358ecd5d9e3290f11a1
Hans-Jörg Merk <github@hmerk.de>
2023-11-22 11:16:48 +0100

    Implement station online check to reduce API calls
    Signed-off-by: Hans-Jörg Merk <github@hmerk.de>

--- bundles/org.openhab.binding.opensmartcity/src/main/java/org/openhab/binding/opensmartcity/internal/OpenSmartCityWeatherHandler.java
+++ bundles/org.openhab.binding.opensmartcity/src/main/java/org/openhab/binding/opensmartcity/internal/OpenSmartCityWeatherHandler.java
@@ -56,8 +56,6 @@ import com.google.gson.JsonObject;
 @NonNullByDefault
 public class OpenSmartCityWeatherHandler extends BaseThingHandler {
 
-    private static final String QUERY_PATH = "/v1.1/Datastreams?%24top=1&%24expand=Observations(%24orderby%3DphenomenonTime%20desc%3B%24filter%3Dday(now())%20sub%20day(phenomenonTime)%20le%201%20and%20month(now())%20eq%20month(phenomenonTime))&%24filter=substringof(%27lufttemperatur%27%2Cname)%20and%20Thing%2Fproperties%2Fstatus%20eq%20%27online%27%20and%20Thing%2FLocations%2Fname%20eq%20%27Dorper%20Stra%C3%9Fe%20%2F%20Goerdeler%20Stra%C3%9Fe%27";
-
     private static final String QUERY_LOCATIONS_ONLINE = "/v1.1/Locations?%24expand=Things&%24filter=substringof(%27online%27%2C%20Things%2Fproperties%2Fstatus)";
 
     private final Logger logger = LoggerFactory.getLogger(OpenSmartCityWeatherHandler.class);
@@ -71,6 +69,9 @@ public class OpenSmartCityWeatherHandler extends BaseThingHandler {
     private Gson gson = new Gson();
 
     private String myLoc = "";
+
+    private String nearestStationId = "";
+    private String nearestStationIdStatus = "offline";
 
     private Map<String, Double> onlineLocations = new HashMap<>();
     private Map<String, String> locationNames = new HashMap<>();
@@ -115,67 +116,51 @@ public class OpenSmartCityWeatherHandler extends BaseThingHandler {
 
     private void updateSensorValues() {
 
-        String nearestStationId = getNearestOnlineLocation();
+        String stationOnlineCheckUrl = "v1.1/Things(33)?$select=properties/status";
+        String stationOnlineCheck = apiRequest(stationOnlineCheckUrl);
+        if (!stationOnlineCheck.contains("online")) {
+            nearestStationIdStatus = "offline";
+        }
+        if (nearestStationId.isEmpty() || !("online".equals(nearestStationIdStatus))) {
+            nearestStationId = getNearestOnlineLocation();
+            nearestStationIdStatus = "online";
+        }
+
         String queryNearestStation = "/v1.1/Datastreams?%24expand=Thing%2C%20Observations(%24top%3D1%3B%24orderby%3DphenomenonTime%20desc)&%24filter=Thing%2F%40iot.id%20eq%20"
                 + nearestStationId
                 + "%20and%20phenomenonTime%20ne%20null%20and%20(substringof(%27lufttemperatur%27%2Cname)%20or%20substringof(%27relative_luftfeuchte%27%2Cname))";
 
-        try {
-            String url = bridgeHandler.basePath + queryNearestStation;
-            logger.debug("Requesting {}", url);
-
-            Request request = bridgeHandler.httpClient.newRequest(url);
-
-            // TODO: remove hard coded credentials
-            request.getHeaders().add("Authorization",
-                    "Basic " + Base64.getEncoder().encodeToString(("smarthomeuser:Solingen2030!").getBytes()));
-            ContentResponse response = request.send();
-
-            if (response.getStatus() == 200) {
-                updateStatus(ThingStatus.ONLINE);
-
-                String content = response.getContentAsString();
-                if (content != null) {
-                    JsonObject jsonResponse = gson.fromJson(content, JsonObject.class);
-                    if (jsonResponse != null) {
-                        logger.debug("Response: {}", jsonResponse.toString());
-                        JsonElement observations = jsonResponse.get("value").getAsJsonArray().get(0);
-                        String rs = observations.toString();
-                        JsonObject jsonResult = gson.fromJson(rs, JsonObject.class);
-                        if (jsonResult != null) {
-                            JsonElement result = jsonResult.get("Observations").getAsJsonArray().get(0);
-                            JsonObject valueObj = gson.fromJson(result.toString(), JsonObject.class);
-                            if (valueObj != null) {
-                                JsonElement value = valueObj.get("result");
-                                logger.debug("Temperature of nearest station: {}", value);
-                                Double temperature = value.getAsBigDecimal().doubleValue();
-                                updateState(OpenSmartCityBindingConstants.CHANNEL_TEMPERATURE,
-                                        new QuantityType<Temperature>(temperature, SIUnits.CELSIUS));
-                            }
-                        }
-                        observations = jsonResponse.get("value").getAsJsonArray().get(1);
-                        rs = observations.toString();
-                        jsonResult = gson.fromJson(rs, JsonObject.class);
-                        if (jsonResult != null) {
-                            JsonElement result = jsonResult.get("Observations").getAsJsonArray().get(0);
-                            JsonObject valueObj = gson.fromJson(result.toString(), JsonObject.class);
-                            if (valueObj != null) {
-                                JsonElement value = valueObj.get("result");
-                                logger.debug("Humidity of nearest station: {}", value);
-                                Double humidity = value.getAsBigDecimal().doubleValue();
-                                updateState(OpenSmartCityBindingConstants.CHANNEL_HUMIDITY, new DecimalType(humidity));
-                            }
-                        }
-                    }
+        String content = apiRequest(queryNearestStation);
+        JsonObject jsonResponse = gson.fromJson(content, JsonObject.class);
+        if (jsonResponse != null) {
+            logger.debug("Response: {}", jsonResponse.toString());
+            JsonElement observations = jsonResponse.get("value").getAsJsonArray().get(0);
+            String rs = observations.toString();
+            JsonObject jsonResult = gson.fromJson(rs, JsonObject.class);
+            if (jsonResult != null) {
+                JsonElement result = jsonResult.get("Observations").getAsJsonArray().get(0);
+                JsonObject valueObj = gson.fromJson(result.toString(), JsonObject.class);
+                if (valueObj != null) {
+                    JsonElement value = valueObj.get("result");
+                    logger.debug("Temperature of nearest station: {}", value);
+                    Double temperature = value.getAsBigDecimal().doubleValue();
+                    updateState(OpenSmartCityBindingConstants.CHANNEL_TEMPERATURE,
+                            new QuantityType<Temperature>(temperature, SIUnits.CELSIUS));
                 }
-            } else {
-                // TODO: check exact problem
-                updateStatus(ThingStatus.OFFLINE);
-                logger.debug("HTTP request failed with response code {}: {}", response.getStatus(),
-                        response.getReason());
             }
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.COMMUNICATION_ERROR);
+            observations = jsonResponse.get("value").getAsJsonArray().get(1);
+            rs = observations.toString();
+            jsonResult = gson.fromJson(rs, JsonObject.class);
+            if (jsonResult != null) {
+                JsonElement result = jsonResult.get("Observations").getAsJsonArray().get(0);
+                JsonObject valueObj = gson.fromJson(result.toString(), JsonObject.class);
+                if (valueObj != null) {
+                    JsonElement value = valueObj.get("result");
+                    logger.debug("Humidity of nearest station: {}", value);
+                    Double humidity = value.getAsBigDecimal().doubleValue();
+                    updateState(OpenSmartCityBindingConstants.CHANNEL_HUMIDITY, new DecimalType(humidity));
+                }
+            }
         }
     }
 
